5dbc20fcf9baf4a5e98438100fdc0fc09bb85fe0
Arpit Tyagi <arpit.tyagi@wnco.com>
2019-07-30 19:31:14 -0500

    Initial Commit
    Initial Commit
    
    Initial Commit

---
+++ src/main/java/com/techgrid/slickbatch/SlickbatchApplication.java
@@ -0,0 +1,13 @@
+package com.techgrid.slickbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlickbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlickbatchApplication.class, args);
+	}
+
+}
